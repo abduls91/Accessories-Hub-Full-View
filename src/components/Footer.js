fcc73169070897ac54b1aaecbc7e32da4ab48ac8
abduls91 <49401146+abduls91@users.noreply.github.com>
2019-04-23 06:22:09 +0500

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+const Footer = props => {
+  return (
+    <footer>
+      <p className="footer-links">
+        <a href="https://www.facebook.com/abdul.s91" target="_blank">
+          Say Hi on Facebook
+        </a>
+      </p>
+      <p>
+        &copy; 2019 <strong>Accessories Hub</strong> - All Rights Reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
